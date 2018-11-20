89df490e882e86c651eee6297c52511182eae9b6
ohigh <ole@kollensoe.dk>
2018-11-20 01:47:40 +0100

    Ole v2

--- Ole/scripts/script.js
+++ Ole/scripts/script.js
@@ -1,28 +1,28 @@
 // default script
 
-$(document).ready(function () {
-	function logoSwitch() {
-		$('.altLogo').each(function () {
-			$(this).css('top',
-				$('.startLogo').offset().top - $(this).closest('.row').offset().top
-			);
-		});
-	}
-
-	$(document).scroll(function () {
-		logoSwitch();
-	});
-
-	logoSwitch();
-
-	// MENU
-
-	$(".burgerMenuBotton").click(function () {
-		$(".burgerMenu").toggleClass("open");
-		$(".burgerMenuBotton").toggleClass("toggle");
-	});
+$(document).ready(function(){
+            function logoSwitch () {
+                $('.altLogo').each(function() {
+                  $(this).css('top',
+                    $('.startLogo').offset().top -  $(this).closest('.page').offset().top
+                  );
+                });
+              }
+              
+              $(document).scroll(function() {logoSwitch();});
+              
+              logoSwitch();
+        
+            // MENU
+            
+                $(".burgerMenuBotton").click(function(){
+                    $(".burgerMenu").toggleClass("open");
+                    $(".burgerMenuBotton").toggleClass("toggle");
+                });
 	
-	
-	
-	
-});
+				$(".burgerLink").click(function(){
+                    $(".burgerMenu").toggleClass("open");
+                    $(".burgerMenuBotton").toggleClass("toggle");
+					
+                });
+            });
