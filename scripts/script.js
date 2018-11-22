9a8b17f45c3c6465cfea39076d5be5ba588e0b41
ohigh <ole@kollensoe.dk>
2018-11-22 11:36:04 +0100

    Ole 3.3.5

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,44 @@
 // default script
 
-$(document).ready(function(){
-            function logoSwitch () {
-                $('.altLogo').each(function() {
-                  $(this).css('top',
-                    $('.startLogo').offset().top -  $(this).closest('.page').offset().top
-                  );
-                });
-              }
-              
-              $(document).scroll(function() {logoSwitch();});
-              
-              logoSwitch();
-        
-            // MENU
-            
-                $(".burgerMenuBotton").click(function(){
-                    $(".burgerMenu").toggleClass("open");
-                    $(".burgerMenuBotton").toggleClass("toggle");
-                });
-	
-				$(".burgerLink").click(function(){
-                    $(".burgerMenu").toggleClass("open");
-                    $(".burgerMenuBotton").toggleClass("toggle");
-					
-                });
-	
-	            $("#callMe").click(function(){
-                    $(".overlay").addClass("on");
-                    
-                });
-    			
-				$("#overlayOff").click(function(){
-                    $(".overlay").removeClass("on");
-                    
-                });
-           
-	
-	
-	
-            });
+$(document).ready(function () {
+	function logoSwitch() {
+		$('.altLogo').each(function () {
+			$(this).css('top',
+				$('.startLogo').offset().top - $(this).closest('.page').offset().top
+			);
+		});
+	}
+
+	$(document).scroll(function () {
+		logoSwitch();
+	});
+
+	logoSwitch();
+
+	// MENU
+
+	$(".burgerMenuBotton").click(function () {
+		$(".burgerMenu").toggleClass("open");
+		$(".burgerMenuBotton").toggleClass("toggle");
+	});
+
+	$(".burgerLink").click(function () {
+		$(".burgerMenu").toggleClass("open");
+		$(".burgerMenuBotton").toggleClass("toggle");
+
+	});
+
+	$("#callMe").click(function () {
+		$(".overlay").addClass("on");
+
+	});
+
+	$("#overlayOff").click(function () {
+		$(".overlay").removeClass("on");
+
+	});
+
+
+
+
+});
